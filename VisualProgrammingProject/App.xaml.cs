8b28b504c25183335c718c8ee5a9f0fb35ab803a
pc <pc@DESKTOP-RSO756K>
2024-11-03 23:55:47 +0100

    Add project files.

---
+++ VisualProgrammingProject/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VisualProgrammingProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
